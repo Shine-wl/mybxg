68b821be7c562280d8837394dae6d801d1584d6c
Shine <571691646@qq.com>
2017-09-21 17:58:22 +0800

    教师模快查看功能实现

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,12 +1,60 @@
-define(['jquery','template'],function ($,template) {
-  console.log(123)
+define(['jquery', 'template','bootstrap'], function ($, template) {
   $.ajax({
-    url:'/api/teacher',
-    type:'get',
-    dataType:'json',
-    success:function(data){
-      var html=template('teacherTpl',{list:data.result});
-      $('#teacherInfo').html(html); 
+    url: '/api/teacher',
+    type: 'get',
+    dataType: 'json',
+    success: function (data) {
+      var html = template('teacherTpl', { list: data.result });
+      $('#teacherInfo').html(html);
+      //注销和启用开始
+      $('.eod').click(function () {
+        var that = this;
+        //获取距离最近的父元素
+        var td = $(this).closest('td');
+        var tcId = td.attr('data-tcId');
+        var tcStatus = td.attr('data-status');
+        // 调用后台数据
+        $.ajax({
+          url: '/api/teacher/handle',
+          type: 'post',
+          data: {
+            tc_id: tcId,
+            tc_status: tcStatus
+          },
+          dataType: 'json',
+          success: function (data) {
+            if (data.code == 200) {
+              td.attr('data-status', data.result.tc_status);
+              if (data.result.tc_status == 0) {
+                $(that).text('启用');
+              } else {
+                $(that).text('注销');
+              }
+            }
+          }
+        })
+      })
+      // 注销和启用结束
+
+      // 查看开始
+      $('.preview').click(function () {
+        var td = $(this).closest('td');
+        var tcId = td.attr('data-tcId');
+        $.ajax({
+          url: '/api/teacher/view',
+          data: { tc_id: tcId },
+          type: 'get',
+          success: function (data) {
+            if (data.code==200) {
+              $('#teacherModal').modal();
+              var html=template('moduleTpl',data.result);
+              $('#moduleInfo').html(html);
+            }
+          }
+        })
+      })
+      // 查看结束
+
     }
   })
 })
