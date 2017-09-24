3296eb75479b18b5a32b515d5abc275a8af3686c
Shine <571691646@qq.com>
2017-09-24 23:18:20 +0800

    完成课程基本信息填充

--- public/js/course-basic.js
+++ public/js/course-basic.js
@@ -1,4 +1,4 @@
-define(['jquery', 'template', 'util'], function ($, template, util) {
+define(['jquery', 'template', 'util', 'ckeditor'], function ($, template, util) {
   // 设置导航菜单选中
   util.setMenu('/course/add');
   var csId = util.qs('cs_id');
@@ -12,12 +12,33 @@ define(['jquery', 'template', 'util'], function ($, template, util) {
     dataType: 'json',
     success: function (data) {
       if (flag) {
-        data.result.operate='编辑课程';
-      }else{
-        data.result.operate='添加课程';
+        data.result.operate = '编辑课程';
+      } else {
+        data.result.operate = '添加课程';
       }
-      var html=template('basicTpl',data.result);
+      var html = template('basicTpl', data.result);
       $('#basicInfo').html(html);
+      $('#firstType').change(function () {
+        // 
+        var pid = $(this).val();
+        $.ajax({
+          type:'get',
+          url: '/api/category/child',
+          data: {
+            cg_id: pid
+          },
+          dataType: 'json',
+          success: function (data) {
+            var tpl = '<option value="">请选择二级分类...</option>' +
+              '{{each list}}' +
+              '<option value="{{$value.cg_id}}"' +
+              '>{{$value.cg_name}}</option>' +
+              '{{/each}}';
+            var html = template.render(tpl, { list: data.result });
+            $('#secondType').html(html);
+          }
+        });
+      });
     }
-  })
-})
+  });
+});
