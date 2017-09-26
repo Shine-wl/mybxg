983a38c2515656debff667989116088c39b90259
Shine <571691646@qq.com>
2017-09-26 10:28:41 +0800

    完成图片裁切功能

--- public/js/config.js
+++ public/js/config.js
@@ -3,15 +3,16 @@ require.config({
   paths: {
     jquery: 'jquery/jquery',
     cookie: 'jquery-cookie/jquery.cookie',
-    template: 'artTemplate/template-web',
+    template: 'artTemplate/template-web',//模板引擎插件
     bootstrap: 'bootstrap/js/bootstrap',
-    datepicker: 'bootstrap-datepicker/js/bootstrap-datepicker',
-    lanugare: 'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
-    validate: 'validate/jquery-validate.min',
-    uploadify: 'upload/jquery.uploadify.min',
+    datepicker: 'bootstrap-datepicker/js/bootstrap-datepicker',//日期插件
+    lanugare: 'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',//日期插件语言设置
+    validate: 'validate/jquery-validate.min',//表单验证插件
+    uploadify: 'upload/jquery.uploadify.min',//文件上传插件
     ckeditor: 'ckeditor/ckeditor', //富文本插件
-    form: 'jquery-form/jquery.form',
-    region: 'jquery-region/jquery.region',
+    form: 'jquery-form/jquery.form',//表单提交插件
+    region: 'jquery-region/jquery.region',//区域选择插件
+    jcrop: 'jcrop/js/jcrop.min',//区域选择插件
     common: '../js/common',
     login: '../js/login',
     util: '../js/util',
@@ -22,7 +23,8 @@ require.config({
     index: '../js/index',
     courseadd: '../js/course-add',
     coursebasic: '../js/course-basic',
-    coursepicture: '../js/course-picture'
+    coursepicture: '../js/course-picture',
+    courselesson: '../js/course-lesson'
   },
   shim: {
     bootstrap: {
@@ -39,6 +41,9 @@ require.config({
     },
     ckeditor: {
       exports: 'CKEDITOR'
+    },
+    jcrop: {
+     deps: ['jquery']
     }
   }
 });
--- public/js/course-picture.js
+++ public/js/course-picture.js
@@ -1,33 +1,94 @@
-define(['jquery','template','util','uploadify'],function ($,template,util) {
+define(['jquery', 'template', 'util', 'uploadify', 'jcrop','form'], function ($, template, util) {
   // 设置导航菜单选中
   util.setMenu('/course/add');
-  var csId=util.qs('cs_id');
+  var csId = util.qs('cs_id');
   $.ajax({
-    url:'/api/course/picture',
-    type:'get',
-    dataType:'json',
-    data:{
-      cs_id:csId
+    url: '/api/course/picture',
+    type: 'get',
+    dataType: 'json',
+    data: {
+      cs_id: csId
     },
-    success:function(data){
-      var html = template('pictureTpl',data.result);
+    success: function (data) {
+      var html = template('pictureTpl', data.result);
       $('#pictureInfo').html(html);
       // 上传图片处理
       $('#myfile').uploadify({
-        width:80,
-        height:'auto',
-        buttonText:'选择图片',
-        itemTemplate:'<span></span>',
-        buttonClass:'btn btn-success btn-sm',
-        swf:'/public/assets/upload/uploadify.swf',
-        fileObjName:'cs_cover_original',
-        uploader:'/api/uploader/cover',
-        formData:{cs_id:csId},
-        onUploadSuccess:function(a,b){
-          var obj=JSON.parse(b)
-          $('.preview img').attr('src',obj.result.path);
+        width: 80,
+        height: 'auto',
+        buttonText: '选择图片',
+        itemTemplate: '<span></span>',
+        buttonClass: 'btn btn-success btn-sm',
+        swf: '/public/assets/upload/uploadify.swf',
+        fileObjName: 'cs_cover_original',
+        uploader: '/api/uploader/cover',
+        formData: { cs_id: csId },
+        onUploadSuccess: function (a, b) {
+          var obj = JSON.parse(b)
+          $('.preview img').attr('src', obj.result.path);
+          // 上传头像成功 裁切后保存
+          $('#cropBtn').text('保存图片').attr('data-flag', true);
+          cropImage();
         }
-      })
+      });
+
+      //获取图片
+      var img = $('.preview img');
+
+      $('#cropBtn').click(function () {
+        var flag = $(this).attr('data-flag');
+        if (flag) {
+          //第二次点击按钮 提交页面
+          $('#cropForm').ajaxSubmit({
+            type:'post',
+            dataType:'json',
+            url:'/api/course/update/picture',
+            data:{cs_id:csId},
+            success:function(data){
+              location.href='/course/lesson';
+            }
+          })
+        } else {
+          //第一次点击按钮
+          $(this).text('保存图片').attr('data-flag', true);
+          cropImage();
+        }
+      });
+      //封装一个独立的方法裁剪图片
+      function cropImage() {
+        img.Jcrop({
+          aspectRatio: 2
+        }, function () {
+          // 设置 将缩略框显示出来
+          $('.thumb').html('');
+          this.initComponent('Thumbnailer', { width: 240, height: 120, mythumb: '.thumb' });
+          $('.jcrop-thumb').css({
+            left:0,
+            top:0
+          })
+          //获取图片的宽度和高度
+          var width = this.ui.stage.width;
+          var height = this.ui.stage.height;
+          var x = 0;
+          var y = (height - width / 2) / 2;
+          var w = width;
+          var h = width / 2;
+          //创建一个选区
+          this.newSelection();
+          this.setSelect([x, y, w, h]);
+
+          img.parent().on('cropastart cropmove cropend', function (a, b, c) {
+            // 获取表单input
+            var aInput=$('#cropForm').find('input');
+            // 向input中添加内容 将选区变化时得到的数据填充到表单内，用来提交给后台，完成裁剪
+            aInput.eq(0).val(c.x);
+            aInput.eq(1).val(c.y);
+            aInput.eq(2).val(c.w);
+            aInput.eq(3).val(c.h);
+          });
+        });
+      }
+
     }
-  })
+  });
 })
